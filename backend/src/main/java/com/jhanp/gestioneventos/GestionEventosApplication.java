a7a87cc371d7b80512980489be4a266170892259
Your Name <you@example.com>
2026-04-14 18:24:13 -0500

    init: estructura inicial del proyecto

---
+++ backend/src/main/java/com/jhanp/gestioneventos/GestionEventosApplication.java
@@ -0,0 +1,14 @@
+package com.jhanp.gestioneventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventosApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GestionEventosApplication.class, args);
+    }
+
+}
+
